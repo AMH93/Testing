a588e246b75309e424a5d4e575961c137e3736a1
AMH93 <103991159+AMH93@users.noreply.github.com>
2023-03-30 13:48:42 +0300

    Create SQLQuery1.sql
    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+SELECT * FROM DetaliiClienti
+SELECT * FROM DetaliiClienti WHERE Oras IS NULL 
+SELECT * FROM DetaliiClienti WHERE [Numar de telefon] IS NULL
+SELECT * FROM DetaliiClienti WHERE Nume IS NULL
+SELECT * FROM DetaliiClienti WHERE [Numar de telefon] IS NULL
